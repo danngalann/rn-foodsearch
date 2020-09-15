26a0129980500784034bceeecddb31fcad5f01de
danngalann <47415745+danngalann@users.noreply.github.com>
2020-09-15 10:17:12 +0200

    Transfer API logic to a hook

---
+++ src/hooks/useResults.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react";
+import yelp from "../api/yelp";
+import * as Location from "expo-location";
+import { Alert } from "react-native";
+
+export default () => {
+  const [results, setResults] = useState([]);
+
+  // Get list of businesses
+  const searchApi = async (term, location) => {
+    try {
+      const { latitude, longitude } = location.coords;
+      const response = await yelp.get("/search", {
+        params: {
+          term,
+          limit: 50,
+          latitude,
+          longitude,
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      Alert.alert(`Something went wrong.`);
+      console.log(e.message);
+    }
+  };
+
+  // Sets geolocation
+  const getLocation = async (callback) => {
+    let { status } = await Location.requestPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access location was denied");
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    if (typeof callback == "function") {
+      callback(location);
+    }
+  };
+
+  // Does a search after updating location
+  const doSearch = (term) => {
+    if (typeof term != "string") {
+      term = "";
+    }
+    getLocation(location => searchApi(term, location));
+  };
+
+  useEffect(() => {
+    doSearch();
+  }, []);
+
+  return [doSearch, results];
+};
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,63 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, Alert } from "react-native";
-import * as Location from "expo-location";
+import { View, Text, StyleSheet } from "react-native";
+
+import useResults from "../hooks/useResults";
 
 import SearchBar from "../components/SearchBar";
 
-import yelp from "../api/yelp";
-
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
+  const [doSearch, results] = useResults();
 
-  // Get list of businesses
-  const searchApi = async (location) => {
-    try {
-      const {latitude, longitude} = location.coords;      
-      const response = await yelp.get("/search", {
-        params: {
-          term,
-          limit: 50,
-          latitude,
-          longitude,
-        },
-      });
-      setResults(response.data.businesses);
-      setTerm("");
-    } catch (e) {
-      Alert.alert(`Something went wrong.`);
-    }
+  const submmit = () => {
+    doSearch(term);
+    setTerm("");
   };
-
-  // Sets geolocation
-  const getLocation = async (callback) => {
-    let { status } = await Location.requestPermissionsAsync();
-    if (status !== "granted") {
-      setErrorMsg("Permission to access location was denied");
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-
-    if (typeof callback == "function") {
-      callback(location);
-    }
-  };
-
-  // Does a search after updating location
-  const doSearch = () => {
-    getLocation(searchApi);
-  };
-
-  useEffect(() => {
-    doSearch();
-  }, []);
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={setTerm}
-        onTermSubmit={doSearch}
+        onTermSubmit={submmit}
       />
       <Text>{JSON.stringify(results[0])}</Text>
     </View>
